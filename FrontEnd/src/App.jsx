bb7119bcbdbde7230057ae0c7fcd822ed8224249
MouadAgn <aoughanemouad1@gmail.com>
2024-11-16 01:54:41 +0100

    Update APP.jsx

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home'; // Vérifiez le chemin d'importation
-import NavBar from './Pages/NavBar/NavBar'; // Si nécessaire
-import Footer from './Pages/Footer/Footer'; // Si nécessaire
 
 function App() {
   return (
